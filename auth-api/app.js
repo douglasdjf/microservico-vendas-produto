9b1ec337c463c7c543c31868292c38f88141bd57
Douglas Fonseca <douglas.vasco79@gmail.com>
2022-08-06 12:57:11 -0300

    Configurando auth

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,15 +1,13 @@
 import express from "express";
 
 import { createInitialData } from "./scr/config/db/initialData.js";
-//import userRoutes from "./scr/modules/user/routes/UserRoutes.js";
-//import tracing from "./scr/config/tracing.js";
+import userRoutes from "./scr/modules/user/routes/UserRoutes.js";
+import tracing from "./scr/config/tracing.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 const CONTAINER_ENV = "container";
-
-createInitialData();
 
 app.get("/api/status", (req, res) => {
   return res.status(200).json({
@@ -19,7 +17,7 @@ app.get("/api/status", (req, res) => {
   });
 });
 
-/*app.use(express.json());
+app.use(express.json());
 
 startApplication();
 
@@ -40,5 +38,3 @@ app.use(userRoutes);
 app.listen(PORT, () => {
   console.info(`Server started successfully at port ${PORT}`);
 });
-
-*/
